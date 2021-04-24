e315ffbe3df68e2dbf80ce57c1f71b389f15269a
Toddy688 <82900504+Toddy688@users.noreply.github.com>
2021-04-24 17:26:41 -0300

    Add files via upload

---
+++ settings/config.js
@@ -0,0 +1,6 @@
+const config = {
+        botName: 'ToinBOT',
+        ownerName: 'Paulo Souza',
+        youtube: 'YOUTUBE_LINK',
+        instagram: 'https://www.instagram.com/pauloczar',
+}
---
+++ src/nabutomenu.js
@@ -0,0 +1,12 @@
+const nabutomenu = (prefix, pushname) => {
+    return `◪ *Comandos do NineX*
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall`
+
+}
+
+exports.nabutomenu = nabutomenu
